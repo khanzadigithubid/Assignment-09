9d2df5e481b579f381c8e80e6585ec10b62c60f1
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2025-01-04 10:36:07 +0600

    Assignment 9

---
+++ src/app/details/[id]/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+
+const ProductDetail = () => {
+    const { id } = useParams(); // Use `useParams` to get the product ID from the URL
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (id) {
+            fetch(`https://fakestoreapi.com/products/${id}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    setProduct(data);
+                    setLoading(false);
+                })
+                .catch((err) => console.error("Error fetching product:", err));
+        }
+    }, [id]);
+
+    if (loading) {
+        return <div className="text-center py-8">Loading...</div>;
+    }
+
+    if (!product) {
+        return <div className="text-center py-8">Product not found</div>;
+    }
+
+    const { image, title, price, description, category } = product;
+
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center py-8 px-4">
+            <div className="max-w-3xl bg-white rounded-lg shadow-lg p-8  items-center gap-8">
+                <img src={image} alt={title} className="w-64 h-64 object-contain" />
+                <div className="text-center md:text-left">
+                    <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
+                    <p className="text-sm text-gray-600 mt-4 mb-6">{description}</p>
+                    <p className="text-lg font-bold text-blue-500">${price}</p>
+                    <span className="inline-block mt-2 text-sm font-bold text-gray-800">
+                        Category: {category}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductDetail;
